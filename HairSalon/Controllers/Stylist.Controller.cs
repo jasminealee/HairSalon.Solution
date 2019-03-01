d4b218b2e9dfe4337f475cf5e8de5c00489476a7
Jasmine Lee <jasmine.al1722@gmail.com>
2019-03-01 13:52:13 -0800

    add directories inside Views directory and edits

---
+++ HairSalon/Controllers/Stylist.Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System;
+
+namespace HairSalon.Controllers
+{
+  public class SpecialityController : Controller
+  {
+  }
+}    
