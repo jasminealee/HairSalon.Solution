ef09f2802dd829f4a5f8f61ac4087373ca523905
Jasmine Lee <jasmine.al1722@gmail.com>
2019-03-08 16:12:56 -0800

    add changes to Stylist.cs, Delete.cshtml,  and New.cshtml

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -93,31 +93,27 @@ namespace HairSalon.Models
     Assert.AreEqual(secondDescription, result);
   }
 
-
-  }
   public void Edit(string newDescription)
-      {
-        MySqlConnection conn = DB.Connection();
-        conn.Open();
-        var cmd = conn.CreateCommand() as MySqlCommand;
-        cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
-        MySqlParameter searchId = new MySqlParameter();
-        searchId.ParameterName = "@searchId";
-        searchId.Value = _id;
-        cmd.Parameters.Add(searchId);
-        MySqlParameter description = new MySqlParameter();
-        description.ParameterName = "@newDescription";
-        description.Value = newDescription;
-        cmd.Parameters.Add(description);
-        cmd.ExecuteNonQuery();
-
-        // More logic will go here.
+  {
+    MySqlConnection conn = DB.Connection();
+    conn.Open();
+    var cmd = conn.CreateCommand() as MySqlCommand;
+    cmd.CommandText = @"UPDATE items SET description = @newDescription WHERE id = @searchId;";
+    MySqlParameter searchId = new MySqlParameter();
+    searchId.ParameterName = "@searchId";
+    searchId.Value = _id;
+    cmd.Parameters.Add(searchId);
+    MySqlParameter description = new MySqlParameter();
+    description.ParameterName = "@newDescription";
+    description.Value = newDescription;
+    cmd.Parameters.Add(description);
+    cmd.ExecuteNonQuery();
 
         conn.Close();
         if (conn != null)
         {
           conn.Dispose();
         }
-      }
+    }
 
 }
